b642a45d8d1e1c3627ab78fb29f82dbfcc9619f1
José Flávio <joseflaviol119@gmail.com>
2022-02-23 17:04:08 -0300

    DEBUG = False

--- bookando_project/settings.py
+++ bookando_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0rxc@k30qpysgupi3pm&b4q!&qsw+9lb#gck($8=53cw6ws@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
